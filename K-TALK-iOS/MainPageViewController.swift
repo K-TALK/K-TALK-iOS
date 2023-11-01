da40ce19246712a45a3c8b6431b459e3832a151b
jisssuu <wkdwltn1015@naver.com>
2023-11-01 13:01:51 +0900

    FEAT : 메인페이지에 로그인 된 이메일 출력

--- K-TALK-iOS/MainPageViewController.swift
+++ K-TALK-iOS/MainPageViewController.swift
@@ -6,24 +6,31 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class MainPageViewController: UIViewController {
-
+    
+    @IBOutlet weak var welcome: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Firebase에서 현재 사용자 정보 가져오기
+        if let currentUser = Auth.auth().currentUser {
+            // 현재 사용자의 이메일 표시
+            welcome.text = "\(currentUser.email!)님 환영합니다!"
+        } else {
+            // 사용자가 로그인하지 않은 경우에 대한 처리
+            welcome.text = "로그인되지 않음"
+        }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
