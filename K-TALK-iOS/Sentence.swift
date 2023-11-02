d822f8dc1b225a791e5ed7afc1576e4df1560d3b
HuMooole <jjj990407@naver.com>
2023-11-03 00:36:02 +0900

    FEAT: realm 추가 및 대본 기능 추가

--- K-TALK-iOS/Sentence.swift
+++ K-TALK-iOS/Sentence.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import RealmSwift
+
+
+class Sentence: Object {
+    @objc dynamic var id:Int = 0
+    @objc dynamic var sentence:String = ""
+    
+    // id 가 고유 값입니다.
+    override static func primaryKey() -> String? {
+      return "id"
+    }
+}
