2d99ed3c1b20f1f611081a83b0b4ce0051f5c661
jisssuu <wkdwltn1015@naver.com>
2023-11-08 20:42:55 +0900

    FEAT : 단어장 검색 기능 구현 & table View 선택 시 단어 세부 정보 보여주는 화면 구성

--- K-TALK-iOS/MainPage2ViewController.swift
+++ K-TALK-iOS/MainPage2ViewController.swift
@@ -7,38 +7,80 @@
 
 import UIKit
 
+
 class MainPage2ViewController: UIViewController {
-    var wordData : [WordData] = []
+    var wordData: [WordData] = []
+    var filteredWord: [WordData] = []
+    let searchController = UISearchController(searchResultsController: nil)
     @IBOutlet weak var wordTable: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         wordData = loadWordDataFromJSON()
+        setUpSearchController()
         wordTable.dataSource = self
-        // Auto layout, variables, and unit scale are not yet supported
+        wordTable.delegate = self
     }
-    let kWord = ["사과", "배", "딸기","포도"]
 
-    /*
-    // MARK: - Navigation
+    private func setUpSearchController() {
+        wordTable.tableHeaderView = searchController.searchBar
+        searchController.searchBar.placeholder = "Search Word"
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.searchBarStyle = .prominent
+        searchController.searchBar.sizeToFit()
+    }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    func showDetailScreen(selectedWord: WordData) {
+        guard let detailViewController = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else {
+            return
+        }
+        detailViewController.selectedWord = selectedWord
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toDetail" {
+            if let destinationViewController = segue.destination as? DetailViewController {
+                if let indexPath = wordTable.indexPathForSelectedRow {
+                    let selectedWord = searchController.isActive ? filteredWord[indexPath.row] : wordData[indexPath.row]
+                    destinationViewController.selectedWord = selectedWord
+                    destinationViewController.modalPresentationStyle = .fullScreen
+                }
+            }
+        }
     }
-    */
-
 }
-extension MainPage2ViewController : UITableViewDataSource {
+
+extension MainPage2ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = wordTable.dequeueReusableCell(withIdentifier: "wordTableCell", for: indexPath) as! wordTableViewCell
-        let word = wordData[indexPath.row]
+        let word: WordData
+        if searchController.isActive && !(searchController.searchBar.text?.isEmpty ?? true) {
+            word = filteredWord[indexPath.row]
+        } else {
+            word = wordData[indexPath.row]
+        }
         cell.korean.text = word.단어
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return wordData.count
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedWord = searchController.isActive && !(searchController.searchBar.text?.isEmpty ?? true) ? filteredWord[indexPath.row] : wordData[indexPath.row]
+        showDetailScreen(selectedWord: selectedWord)
     }
-    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchController.isActive && !(searchController.searchBar.text?.isEmpty ?? true) ? filteredWord.count : wordData.count
+    }
+}
+
+extension MainPage2ViewController: UISearchResultsUpdating {
+    func filteredContentForSearchText(_ searchText: String) {
+        filteredWord = wordData.filter { $0.단어.lowercased().contains(searchText.lowercased()) }
+        wordTable.reloadData()
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredContentForSearchText(searchController.searchBar.text ?? "")
+    }
 }
